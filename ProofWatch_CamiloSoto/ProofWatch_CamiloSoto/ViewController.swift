723b5e6edb349441e883897c673d3aad05eab0d5
CAMILO ANDRES SOTO HERRERA <csoto@pammos.com>
2020-09-21 19:56:44 -0500

    Creacion del proyecto

---
+++ ProofWatch_CamiloSoto/ProofWatch_CamiloSoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProofWatch_CamiloSoto
+//
+//  Created by Camilo Soto on 21/09/20.
+//  Copyright © 2020 CamiloSoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
